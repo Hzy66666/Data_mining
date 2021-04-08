9a746f6952c95a6ee33bfd474bac60ca859e2ab3
Your Name <email@example.com>
2021-04-08 09:41:58 +0800

    update codes

---
+++ config.py
@@ -0,0 +1,5 @@
+data1_path = "Wine"
+data1_file_list = ["winemag-data_first150k.csv","winemag-data-130k-v2.csv"]
+
+data2_path = "Oakland_crime"
+data2_file_list = ["records-for-2011.csv", "records-for-2012.csv", "records-for-2013.csv", "records-for-2014.csv", "records-for-2015.csv", "records-for-2016.csv"]
